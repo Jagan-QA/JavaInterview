b8a26a9f7a3568bc49029342f0cdbeee090348cf
Jagan-QA <110659661+Jagan-QA@users.noreply.github.com>
2022-08-05 13:10:05 +0530

    String Reverse
    This is the Code to Reverse a String using 3 Approaches

---
+++ StrRev.java
@@ -0,0 +1,60 @@
+package javaInterview;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+
+public class StrRev {
+
+	public static void main(String[] args) {
+		
+		
+		/* We can Reverse a String in 3 approaches
+		 * (i) Using String Buffer or String Builder 
+		 * (ii) We can write our own logic
+		 * (iii) We can use ArrayList
+		 */
+	/*	
+		// Approach 1
+		
+		String name = "Jagan";
+		StringBuilder obj = new StringBuilder(name);
+        System.out.println(obj.reverse());
+		
+        // Approach 2
+        //We need to use Charracter array 
+         
+           char [] CharArray= name.toCharArray();
+           int Charlength =CharArray.length;
+           System.out.println(Charlength);
+           System.out.println(name.length());
+           
+            String Reversed="";
+            for(int i=CharArray.length-1; i>=0; i--)
+            {
+            	Reversed = Reversed+CharArray[i];
+               System.out.println(Reversed);
+            }
+            */
+       // We can use Array list also
+		String name = "Jagan";
+            char [] arr1 = name.toCharArray();
+           List<Character> obj1= new ArrayList<Character>();
+           System.out.println(arr1.length);
+           System.out.println(arr1.length-1);
+            for(int k=arr1.length-1; k>=0; k--)
+            {
+            	obj1.add(arr1[k]);
+            	System.out.println(obj1);
+            }
+            ListIterator<Character > obj2 =  obj1.listIterator();
+            while(obj2.hasNext()) {
+            	System.out.println(obj2.next());
+            }
+            System.out.println(obj2.toString());
+            
+           
+	
+           }
+	       }
